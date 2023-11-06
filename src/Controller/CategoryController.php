5f898f1c73b928d26b24423c70176ac988421d1a
plbonnier <pl_bonnier@yahoo.fr>
2023-11-06 14:34:18 +0100

    start S01US10

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,8 +6,16 @@ use App\Model\CategoryManager;
 
 class CategoryController extends AbstractController
 {
+    // public function index(): string
+    // {
+    //     return $this->twig->render('Category/index.html.twig');
+    // }
+
     public function index(): string
     {
-        return $this->twig->render('Category/index.html.twig');
+        $categoryManager = new CategoryManager();
+        $category = $categoryManager->selectAll('title');
+
+        return $this->twig->render('Category/index.html.twig', ['category' => $category]);
     }
 }
