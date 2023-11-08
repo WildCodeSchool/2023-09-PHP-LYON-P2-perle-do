babe968ac9f331d23468600dd00ab8bf8cc06e56
NICKY RASAMOELINA <nickyrasa@gmail.com>
2023-11-08 16:08:53 +0100

    add category first attempt

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,12 +6,36 @@ use App\Model\CategoryManager;
 
 class CategoryController extends AbstractController
 {
-    public function index(): string
+    public function indexCategory(): string
     {
-        return $this->twig->render('Category/index.html.twig');
+        $categoryManager = new CategoryManager();
+        $categories = $categoryManager->selectAll('name');
+
+        return $this->twig->render('Category/index.html.twig', ['categories' => $categories]);
     }
-    public function addCategory(): string
+
+    public function show(int $id): string
     {
+        $categoryManager = new CategoryManager();
+        $item = $categoryManager->selectOneById($id);
+
+        return $this->twig->render('Category/index.html.twig', ['item' => $item]);
+    }
+    public function addCategory(): ?string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $category = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, insert and redirection
+            $categoryManager = new CategoryManager();
+            $category = $categoryManager->addCategory($category);
+
+            header('Location:/categories/');
+            return null;
+        }
         return $this->twig->render('Category/add.html.twig');
     }
 }
--- src/Model/CategoryManager.php
+++ src/Model/CategoryManager.php
@@ -6,4 +6,13 @@ use PDO;
 
 class CategoryManager extends AbstractManager
 {
+    public const TABLE = 'category';
+
+    public function addCategory(array $category): int
+    {
+            $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . "(`name`) VALUES (:name)");
+            $statement->bindValue(':name', $category['name'], PDO::PARAM_STR);
+            $statement->execute();
+             return (int)$this->pdo->lastInsertId();
+    }
 }
