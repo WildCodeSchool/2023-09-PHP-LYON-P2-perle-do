09b9f090426aa87e890fc9582fd405453c11fef8
Romain GUIOT <romain.guiot@hotmail.fr>
2023-11-08 13:30:09 +0100

    update

--- public/index.php
+++ public/index.php
@@ -6,8 +6,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/../config/debug.php';
 
-if (file_exists(__DIR__ . '/../config/db.php')) 
-{
+if (file_exists(__DIR__ . '/../config/db.php')) {
     require_once __DIR__ . '/../config/db.php';
 }
 
