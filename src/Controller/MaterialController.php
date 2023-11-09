bf4218bf52f1f491ee5f8b18775fa3aa43ede6ac
NICKY RASAMOELINA <nickyrasa@gmail.com>
2023-11-09 17:28:48 +0100

    first commit

--- src/Controller/MaterialController.php
+++ src/Controller/MaterialController.php
@@ -6,8 +6,20 @@ use App\Model\MaterialManager;
 
 class MaterialController extends AbstractController
 {
-    public function index(): string
+    public function indexMaterial(int $id): string
     {
-        return $this->twig->render('Material/index.html.twig');
+        $materialManager = new MaterialManager();
+        $materials = $materialManager -> getMaterialById($id);
+
+        return $this->twig->render('Material/index.html.twig', ['materials' => $materials]);
     }
+
+    //   public function showMaterial(int $id): string
+    // {
+    //     $materialManager = new MaterialManager();
+    //     $material = $materialManager->getMaterialById($id);
+    //     return $this->twig->render('Customer/show.html.twig', [
+    //         'material' => $material,
+    //     ]);
+    // }
 }
--- src/Model/MaterialManager.php
+++ src/Model/MaterialManager.php
@@ -6,4 +6,32 @@ use PDO;
 
 class MaterialManager extends AbstractManager
 {
+    public const TABLE = 'material';
+
+    public function getAllMaterial(): array|false
+    {
+            $sql = 'SELECT DISTINCT m.name materialName, c.name category FROM material as m
+                JOIN product as p ON m.id = p.id_material
+                JOIN category as c ON c.id = p.id_category
+                WHERE c.id=p.id_category';
+
+            $query = $this->pdo->prepare($sql);
+            $query->execute();
+            $materials =  $query->fetchAll(PDO::FETCH_ASSOC);
+
+            return $materials;
+    }
+    public function getMaterialById(int $id): array|false
+    {
+            $sql = 'SELECT DISTINCT m.name, c.name FROM material as m
+                JOIN product as p ON m.id = p.id_material
+                JOIN category as c ON c.id = p.id_category
+                WHERE c.id=:p.id_category';
+
+            $query = $this->pdo->prepare($sql);
+            $query->bindValue('id', $id, PDO::PARAM_INT);
+            $query->execute();
+
+            return $query->fetch();
+    }
 }
