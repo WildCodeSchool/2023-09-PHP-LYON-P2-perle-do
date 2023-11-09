052f66aa86b97c890cf7f0139012c1958badc9af
plbonnier <pl_bonnier@yahoo.fr>
2023-11-09 10:54:26 +0100

    create edit function and link page show to edit

--- src/Service/ValidationService.php
+++ src/Service/ValidationService.php
@@ -34,12 +34,16 @@ class ValidationService
         if (strlen($customer['city']) > 50) {
             $this->errors[] = "Le nom de la ville est trop long";
         }
-        if (!preg_match('/[0-9]{5}/', $customer['zipcode'])) {
-            $this->errors[] = "Le numéro n\'est pas au bon format";
+        if (!empty($customer['zipcode']) && !preg_match('/[0-9]{5}/', $customer['zipcode'])) {
+            $this->errors[] = "Le CP n\'est pas au bon format";
         }
-        if (!filter_var($customer['email'], FILTER_VALIDATE_EMAIL)) {
+        if (!empty($customer['email']) && !filter_var($customer['email'], FILTER_VALIDATE_EMAIL)) {
             $this->errors[] = "L'adresse mail n'est pas au bon format";
         }
+    }
+
+    public function formValidationCustomer3($customer): void
+    {
         if (!empty($customer['phone']) && !preg_match('/[0-9]{10}/', $customer['phone'])) {
             $this->errors[] = "Le numéro de téléphone n\'est pas au bon format";
         }
