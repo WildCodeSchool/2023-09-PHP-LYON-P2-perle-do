5be748a3b494768aa9e7391c8939e6dc2c7f31d2
plbonnier <pl_bonnier@yahoo.fr>
2023-11-12 20:11:47 +0100

    button + - cartshopping js and add in cart in a next feature

--- src/Model/ProductManager.php
+++ src/Model/ProductManager.php
@@ -9,7 +9,8 @@ class ProductManager extends AbstractManager
     public function getProductsByCategoryAndMaterial($categoryId, $materialId): array|false
     {
         $sql = "SELECT p.name AS productName, c.id AS categoryId, m.id AS materialId, p.price, 
-        p.reference AS referenceProduct, c.name AS categoryName, m.name AS materialName FROM product p
+        p.reference AS referenceProduct, c.name AS categoryName, m.name AS materialName, p.description
+        FROM product p
         JOIN category c ON c.id=p.id_category
         JOIN material m ON m.id=p.id_material
         WHERE c.id=:categoryId AND m.id=:materialId";
