11122603413f4f64ae3b582557329e8438a9fe6a
plbonnier <pl_bonnier@yahoo.fr>
2023-11-16 16:59:56 +0100

    create show html twig and controller

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -3,16 +3,30 @@
 namespace App\Controller;
 
 use App\Model\InvoiceManager;
+use Pdf;
 
 class InvoiceController extends AbstractController
 {
-    public function index(): string
+    public function indexInvoice(): string
     {
         if (isset($_SESSION['user_id'])) {
-            return $this->twig->render('Customer/index.html.twig');
+            return $this->twig->render('Invoice/index.html.twig');
         } else {
             header('Location: /');
             die();
         }
     }
+
+    // public function showInvoice($id): void
+    // {
+    //     if (isset($_SESSION['user_id'])) {
+    //         $invoiceManager = new InvoiceManager();
+    //         // $invoice = $invoiceManager->selectOneById($id);
+    //         // $PDF = new \mikehaertl\wkhtmlto\Pdf($invoice);
+    //         // $PDF->send();
+    //     } else {
+    //         header('Location: /');
+    //         die();
+    //     }
+    // }
 }
