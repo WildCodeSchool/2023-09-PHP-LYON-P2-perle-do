60fa13f8ce57c9446bc769bdba867764d6c4cc29
nicky <nickyrasa@gmail.com>
2023-11-18 00:15:23 +0100

    calculator

--- public/assets/script.js
+++ public/assets/script.js
@@ -126,3 +126,26 @@ buttons.forEach((button) => {
     });
 });
 
+
+const display = document.querySelector(".display");
+const spans = document.querySelectorAll("span");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+const calculate = (btnValue) => {
+    if (btnValue === "=" && output !== "") {
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "AC") {
+        output = "";
+    } else if (btnValue === "DEL") {
+        output = output.toString().slice(0, -1);
+    } else {
+        if (output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+};
+spans.forEach((buttons) => {
+    buttons.addEventListener("click", e => calculate(e.target.dataset.value))
+});
+
